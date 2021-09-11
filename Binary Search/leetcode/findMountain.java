c43451c1cdb779b9390e2ad7225304325f04dc37
nikhilvaibhav <nikhilvaibhav545@gmail.com>
2021-09-11 12:00:40 +0530

    few change

---
+++ Binary Search/leetcode/findMountain.java
@@ -0,0 +1,17 @@
+public class findMountain{
+
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,2,1};
+        int target = 2;
+    }
+
+    static int find(int arr[], int target){
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start = (end - start)/2;
+            if()
+        }
+
+    }
+}
--- Binary Search/leetcode/peakIndex.java
+++ Binary Search/leetcode/peakIndex.java
@@ -1,24 +1,22 @@
 public class peakIndex{
     public static void main(String[] args) {
-        int arr[] = {0,1,2,10,5,2,1};
-        System.out.print(peak(arr));
+        int arr[] = {1,2,3,4,3,1,0};
+        System.out.print(find(arr));
     }
-    static int peak(int arr[]){
-        int start = 1;
+
+    static int find(int arr[]){
+        int start = 0;
         int end = arr.length - 1;
 
-        while(start <= end){
+        while(start < end){
             int mid = start + (end - start)/2;
-            if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]){
-                return mid;
-            }
-            if (arr[mid] < arr[mid + 1]){
-                start = mid + 1;
+            if(arr[mid] > arr[mid + 1]){
+                end = mid;
             }
             else{
-                end = mid - 1;
+                start = mid + 1;
             }
         }
-        return -1;
+        return start;
     }
 }
