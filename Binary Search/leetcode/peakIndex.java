f7d80e71db0d7d4f9a79747a23f4c0c5ea88fcf3
nikhilvaibhav <nikhilvaibhav545@gmail.com>
2021-09-15 20:03:45 +0530

    minor change 15th sept

--- Binary Search/leetcode/peakIndex.java
+++ Binary Search/leetcode/peakIndex.java
@@ -1,24 +1,27 @@
 public class peakIndex{
     public static void main(String[] args) {
-        int arr[] = {1,2,4,3,1,0};
+        int arr[] = {1,2,4,5,3,1,0};
         System.out.print(find(arr));
     }
 
     static int find(int arr[]){
         int start = 0;
         int end = arr.length - 1;
-        int n = arr.length;
 
-            while(start < end){
-                int mid = start + (end - start)/2;
-                if(arr[mid] < arr[mid + 1]){
-                    start = mid + 1;
+            while(start <= end){
+                int mid = start + ( end - start)/2;
+                if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]){
+                    return mid;
                 }
-                else{
+
+                if(arr[mid] > arr[mid + 1]){
                     end = mid;
                 }
+                else{
+                    start = mid + 1;
+                }
             }
-            return start;
+            return end;
     }
     
 }
